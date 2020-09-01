ed264244f79a07ffecab858ff558c87213fa29e7
chalk-pen <aleksandr.editor@gmail.com>
2020-09-02 01:09:56 +0300

    create function

--- objectRenamer.py
+++ objectRenamer.py
@@ -8,42 +8,43 @@ SUFFIXES = {
 
 DEFAULT_SUFFIX = "grp"
 
-selection = cmds.ls(sl=True)
+def rename():
+    selection = cmds.ls(sl=True)
 
-if len(selection) == 0:
-    selection = cmds.ls(dag=True, long=True)
+    if len(selection) == 0:
+        selection = cmds.ls(dag=True, long=True)
 
-selection.sort(key=len, reverse=True)
+    selection.sort(key=len, reverse=True)
 
-for obj in selection:
-    short_name = obj.split("|")[-1]
+    for obj in selection:
+        short_name = obj.split("|")[-1]
 
-    children = cmds.listRelatives(obj, children=True, fullPath=True) or []
+        children = cmds.listRelatives(obj, children=True, fullPath=True) or []
 
-    if len(children) == 1:
-        child = children[0]
-        objType = cmds.objectType(child)
-    else:
-        objType = cmds.objectType(obj)
+        if len(children) == 1:
+            child = children[0]
+            objType = cmds.objectType(child)
+        else:
+            objType = cmds.objectType(obj)
 
-    if objType == "mesh":
-        suffix = "geo"
-    elif objType == "joint":
-        suffix == "jnt"
-    elif objType == "camera":
-        print "Skipping camera"
-        continue
-    else:
-        suffix = "grp"
+        if objType == "mesh":
+            suffix = "geo"
+        elif objType == "joint":
+            suffix == "jnt"
+        elif objType == "camera":
+            print "Skipping camera"
+            continue
+        else:
+            suffix = "grp"
 
-    suffix = SUFFIXES.get(objType, DEFAULT_SUFFIX)
+        suffix = SUFFIXES.get(objType, DEFAULT_SUFFIX)
 
-    if not suffix:
-        continue
+        if not suffix:
+            continue
 
-    if obj.endswith(suffix):
-        continue
+        if obj.endswith(suffix):
+            continue
 
-    newName = short_name + "_" + suffix
+        newName = short_name + "_" + suffix
 
-    cmds.rename(obj, newName)
+        cmds.rename(obj, newName)
